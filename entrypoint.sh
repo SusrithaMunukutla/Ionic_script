1febfb232e54fd91005273ff1408a6891b2f0121
samhitha30 <samhitha225@gmail.com>
2016-05-03 17:54:49 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,16 @@ git clone https://github.com/SusrithaMunukutla/Sensor-app.git /application
 cd /application
 unzip sensor.zip
 cp -r /application/sensor/platforms/android/build/outputs/apk /application
-#git init
-#git add .
-#git commit -m "First commit"
-#git remote add testt https://github.com/samhitha30/Sensor-app.git
-#git push -u testt master
+git remote set-url origin https://SusrithaMunukutla:welcome123@github.com/SusrithaMunukutla/Sensor-app.git
+git init
+git add .
+git config --global user.email "samhitha225@gmail.com"
+git config --global user.nsme "samhitha30"
+git commit -m "First commit"
+git remote -v
+git push origin master
 cd /application/sensor
 ionic serve -a
 #if(curl http://52.63.237.171:8100); then echo"Successful"; else echo"Failed";
 #fi
-
+ 
