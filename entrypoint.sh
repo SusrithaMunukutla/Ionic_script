22232f4fdd7f9208ce1d884cd01402276fa571b4
samhitha30 <samhitha225@gmail.com>
2016-05-03 23:04:04 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ unzip sensor.zip
 cp -r /application/sensor/platforms/android/build/outputs/apk/android-debug.apk /application
 git remote set-url origin https://samhitha30:github123@github.com/samhitha30/ionic-app.git
 git init
-git add .
+git add android-debug.apk
 git config --global user.email "samhitha225@gmail.com"
 git config --global user.name "samhitha30"
 git commit -m "First commit"
