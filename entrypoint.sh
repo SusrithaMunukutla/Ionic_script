a21e3f78ae786c210c4a1ecf8f49094cb2fa1473
samhitha30 <samhitha225@gmail.com>
2016-05-05 09:19:11 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,10 @@
 git clone https://github.com/samhitha30/ionic-app.git /application
 cd /application
 unzip sensor.zip
-cd /sensor
+cd sensor
 ionic build android
 cp /application/sensor/platforms/android/build/outputs/apk/android-debug.apk /application
+cd ..
 git remote set-url origin https://samhitha30:github123@github.com/samhitha30/ionic-app.git
 git init
 git config --global user.email "samhitha225@gmail.com"
@@ -13,10 +14,10 @@ git config --global user.name "samhitha30"
 #git commit -m 'removed'
 #git push
 git add -u android-debug.apk
-git commit -m 'added'
+git commit -m 'added apk'
 git remote -v
 git push
-cd application/sensor
+cd /application/sensor
 ionic serve -a
 #if(curl http://52.63.237.171:8100); then echo"Successful"; else echo"Failed";
 #fi
